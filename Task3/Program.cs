93ffe046cc6347e4c269f124d73c11ae087f03be
Sergey Petrovsky <sgp-61@mail.ru>
2024-04-27 22:31:32 +0300

    Sem# Task2.3

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+//  Использовать рекурсию, не использовать циклы.
+
+void Len(int num)
+{
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    Len`(num - 1);
+}
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Len(n);
+
+int[] num = [1, 2, 5, 0, 10, 34];

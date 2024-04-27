d0be3d8acd3e2bda9ede3db7093c8160e227787c
Sergey Petrovsky <sgp-61@mail.ru>
2024-04-27 21:47:34 +0300

    Sem# Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке 
+// от M до N. Использовать рекурсию, не использовать циклы.
+
+void FromMtoN(int m, int n)
+{
+if (m > n)
+{
+FromMtoN(m - 1, n);
+System.Console.Write(m + " ");
+}
+else if (n > m)
+{
+FromMtoN(m, n - 1);
+System.Console.Write(n + " ");
+}
+else System.Console.Write(m + " ");
+}
+FromMtoN(2, 8);
